658de103b1a044d447741ce685e6f98f8659d6c4
cocoao <cocoaooo212@gmail.com>
2021-02-09 12:54:56 +0900

    210209 메인 그래프, 서브 그래프 완료 / 반응형 완료

--- index.php
+++ index.php
@@ -4,10 +4,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Scehdule Dashboard</title>
+
+  <!-- font awesome font link -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+  <!-- css style link -->
   <link rel="stylesheet" href="/my_schedule/css/reset.css">
   <link rel="stylesheet" href="/my_schedule/css/style.css">
+  <link rel="stylesheet" href="/my_schedule/css/media.css">
+
+  <!-- script file load  -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/jquery.easypiechart.min.js"></script>
+  <script src="/my_schedule/js/custom.js"></script>
   <script src="/my_schedule/js/piechart.js"></script>
 </head>
 <body>
@@ -37,13 +46,58 @@
             <p class="totalTit">Total Process Rates</p>
           </section>
         </div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
+        <div class="item subPfBar">
+          <div class="pfImg">
+            <i class="fa fa-database"></i>
+          </div>
+          <div class="pfContents">
+            <h3>Database Project</h3>
+            <p>Progress Rate : <b class="rateNum">90</b>%  |  Last Update : <b>2021-02-09</b></p>
+            <div class="pfBarBox">
+              <span class="pfBar"></span>
+            </div>
+          </div>
+        </div>
+        <div class="item subPfBar">
+          <div class="pfImg">
+            <i class="fa fa-thermometer-half"></i>
+          </div>
+          <div class="pfContents">
+            <h3>API Project</h3>
+            <p>Progress Rate : <b class="rateNum">70</b>%  |  Last Update : <b>2021-02-09</b></p>
+            <div class="pfBarBox">
+              <span class="pfBar"></span>
+            </div>
+          </div>
+        </div>
+        <div class="item subPfBar">
+          <div class="pfImg">
+            <i class="fa fa-clone"></i>
+          </div>
+          <div class="pfContents">
+            <h3>Renewal Project</h3>
+            <p>Progress Rate : <b class="rateNum">30</b>%  |  Last Update : <b>2021-02-09</b></p>
+            <div class="pfBarBox">
+              <span class="pfBar"></span>
+            </div>
+          </div>
+        </div>
+        <div class="item subPfBar">
+          <div class="pfImg">
+            <i class="fa fa-bar-chart-o"></i>
+          </div>
+          <div class="pfContents">
+            <h3>Web pranning Project</h3>
+            <p>Progress Rate : <b class="rateNum">10</b>%  |  Last Update : <b>2021-02-09</b></p>
+            <div class="pfBarBox">
+              <span class="pfBar"></span>
+            </div>
+          </div>
+        </div>
         <div class="item"></div>
       </div>
     </div>
+    <footer></footer>
   </div>
 </body>
 </html>
--- js/piechart.js
+++ js/piechart.js
@@ -1,11 +1,46 @@
 $(function() {
+
+  let pieSize = 250;
+  let liWidth = $(window).width();
+  let clear;
+
+  if(liWidth > 401){
+    pieSize = 250;
+  } else {
+    pieSize = 180;
+  };
+
   $('.circle-graph').easyPieChart({
     scaleColor: false,
     lineWidth: 40,
     lineCap: 'round',
     barColor: '#4298ff',
-    trackColor: '#cccccc' ,
-    size: 250,
-    animate: 800
+    trackColor: '#ddd' ,
+    size: pieSize,
+    animate: 1200
+  });
+
+  $(window).resize(function(){
+    let liWidth = $(window).width();
+    if(liWidth > 401){
+      pieSize = 250;
+    } else {
+      pieSize = 180;
+    };
+
+    // clearTimeout(clear);
+
+    clear=setTimeout(function(){
+      $('.circle-graph').removeData('easyPieChart').find('canvas').remove();
+      $('.circle-graph').easyPieChart({
+        scaleColor: false,
+        lineWidth: 40,
+        lineCap: 'round',
+        barColor: '#4298ff',
+        trackColor: '#ddd' ,
+        size: pieSize,
+        animate: 1
+      });
+    }, 150);
   });
 });
